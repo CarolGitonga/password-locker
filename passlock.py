1d43cca128e811a511edbc57828ae6bde9236095
carol-profile <carolgitonga93@gmail.com>
2022-04-23 20:02:11 +0300

    create Credentials class

--- passlock.py
+++ passlock.py
@@ -1,3 +1,4 @@
+import random
 class User:
     """
     Create User class that generates new instances of a user.
@@ -11,5 +12,46 @@ class User:
         method that defines the properties of a user.
         """
         self.username = username
+        self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instance into the user list
+        """
+        User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+        '''
+        delete_account method deletes a  saved account from the list
+        '''
+        User.user_list.remove(self)
+
+class Credentials():
+    """
+    Create credentials class to help create new objects of credentials
+    """
+    credentials_list = []
+
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
         self.password = password
         
